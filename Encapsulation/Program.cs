6af3b0a7d1b3ef29b316d1ddd25ce9ce947f5949
Marius Gžegoževskis <m.gzegozevskis@ad.viko.lt>
2022-01-17 22:27:02 +0200

    encapsulation/abstraction

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Encapsulation
+{
+  // Step 1: What Customer class need ( ABSTRACTION )
+
+  // Step 2: How Validate and CreateDBObjects
+
+  // Step 3: Made those complicated methods as private ( ENCAPSUPLATION )
+  public class Customer
+  {
+    public string Code = "";
+    public string Name = "";
+    public void Add()
+    {
+
+    }
+    public bool Validate()
+    {
+      return true;
+    }
+    public bool CreateDBObjects()
+    {
+      return true;
+    }
+  }
+
+  public class CustomerWithEncapsulation
+  {
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public void Add()
+    {
+      Validate();
+      CreateDBObjects();
+    }
+    private bool Validate()
+    {
+      return true;
+    }
+    private bool CreateDBObjects()
+    {
+      return true;
+    }
+  }
+  public class Program
+  {
+    static void Main() { 
+    }
+  }
+}
---
+++ EncapsulationMain/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EncapsulationMain {
+    class Program 
+    { 
+      static void Main(string[] args)
+      {
+        Encapsulation.Customer customer = new Encapsulation.Customer();
+        customer.Name = "Marius";
+        customer.Code = "s007";
+
+        // Is complicated for user which try to implement something using Encapsulation component
+        // Why I need to use and when Validate, CreateDBObjects and other methods???
+        customer.Validate();
+        customer.CreateDBObjects();
+        customer.Add();
+
+      Encapsulation.CustomerWithEncapsulation customerEncapsulated = new Encapsulation.CustomerWithEncapsulation();
+      
+      // You exposing data which is important and easier to managing permissions / data hiding process
+      // You don't need to see or call all methods which is implemented inside CustomerWithEncapsulation class.
+      customerEncapsulated.Name = "Tom";
+      customerEncapsulated.Code = "s021315";
+      customerEncapsulated.Add();
+      
+
+
+    }
+  }
+}
+
