4468e4763173476d979e1d476a41800d4472dd75
Marius Gžegoževskis <m.gzegozevskis@ad.viko.lt>
2022-01-22 20:33:49 +0200

    Single Responsibility Principle (SRP)

---
+++ SOLID_DRY_KISS/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace SOLID_DRY_KISS { 
+/*
+===========Intro to SOLID principles===========
+SOLID principles are the design principles that enable us to manage most of the software design problems. Robert C. Martin compiled these principles in the 1990s. These principles provide us with ways to move from tightly coupled code and little encapsulation to the desired results of loosely coupled and encapsulated real needs of a business properly. SOLID is an acronym of the following.
+
+S: Single Responsibility Principle (SRP)
+O: Open closed Principle (OCP)
+L: Liskov substitution Principle (LSP)
+I: Interface Segregation Principle (ISP)
+D: Dependency Inversion Principle (DIP)
+
+*/
+
+/*
+ =========DRY==========:
+ * In software engineering:
+ * Don 't Repeat Yourself (DRY) 
+ * Duplication is Evil (DIE) is a principle of software development.
+
+ =========KISS=========:
+ * KISS is an acronym for the design principle "Keep it simple, Stupid!".
+
+ */
+
+
+// See https://aka.ms/new-console-template for more information
+
+public class Program {
+    static void Main()
+    {
+      // EASY WAY :)
+      BlizzardAppService blizzardAppService = new BlizzardAppService();
+      blizzardAppService.AddFriend("marius#444");
+
+
+      // Single Responsibility Principle (SRP)
+      RequestFriendClient requestFriendClient = new RequestFriendClient(); 
+      BlizzardAppServiceSRP blizzard = new BlizzardAppServiceSRP(new BlizzardAppServiceSRP.FriendRequestService(requestFriendClient));
+      blizzard.AddFriend("Marius#2222");
+
+    }
+  }
+}
---
+++ SOLID_DRY_KISS/WithoutSingleResponsibility.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_DRY_KISS
+{
+
+  // CORE CLASSES FOR BOTH SOLUTIONS
+  public class RequestFriendClient
+  {
+    public void Send(string battleTag)
+    {
+      // DUMMY METHOD
+      // IN REAL WORLD CALLING API REQUEST OR QUERY FROM DB 
+      Console.WriteLine($"Friend request {battleTag} was successfully sended!");
+    }
+  }
+
+  // END OF CORE CLASSES 
+
+  public class WithoutSingleResponsibility
+  {
+
+  }
+  public class BlizzardAppService
+  {
+    public void AddFriend(string battleTag) {
+      if (!ValidateBattleTag(battleTag))
+      {
+        throw new Exception("Wrong email or battleTag");
+      }
+      else
+      {
+        SendFriendRequest(battleTag);
+      }
+
+    }
+    public virtual bool ValidateBattleTag(string battleTag) {
+      // Validation is for demo only you can implement regex or other validate algorithm
+      return battleTag.Contains("#");
+    }
+    public void SendFriendRequest(string battleTag)
+    {
+      // Also you can call other class, server api call and so on
+      Console.WriteLine($"Friend request {battleTag} was successfully sended!");
+    }
+  }
+  // S: Single Responsibility Principle (SRP)
+  // Same app different approach
+  public class BlizzardAppServiceSRP
+  {
+    FriendRequestService _friendRequestService;
+    // You can add any Database context class or other props 
+    public BlizzardAppServiceSRP(FriendRequestService friendRequestService)
+    {
+      _friendRequestService = friendRequestService;
+    }
+    public void AddFriend(string battleTag)
+    {
+      if (!_friendRequestService.ValidateBattleTag(battleTag))
+      {
+        throw new Exception("Wrong email or battleTag");
+      }
+      else
+      {
+        _friendRequestService.SendFriendRequest(battleTag);
+      }
+    }
+    public class FriendRequestService
+    {
+      RequestFriendClient _requestFriendClient;
+
+      public FriendRequestService(RequestFriendClient requestFriendClient)
+      {
+        _requestFriendClient = requestFriendClient;
+      }
+
+      public virtual bool ValidateBattleTag(string battleTag)
+      {
+        // Validation is for demo only you can implement regex or other validate algorithm
+        return battleTag.Contains("#");
+      }
+      public void SendFriendRequest(string battleTag)
+      {
+        // Also you can call other class, server api call and so on
+        _requestFriendClient.Send(battleTag);
+      }
+    }
+  }
+}
